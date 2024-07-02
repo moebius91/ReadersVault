625ed1cadcf82ff4abf8d1e623f761ea3f292bf2
moebius91 <fb.othersen@gmail.com>
2024-07-02 13:09:10 +0200

    HomeView Design mit Testinhalt

--- BookVault/BookVault/HomeView/HomeView.swift
+++ BookVault/BookVault/HomeView/HomeView.swift
@@ -7,9 +7,75 @@
 
 import SwiftUI
 
+let bookList: [String] = ["Buch 1", "Buch 2", "Buch 3", "Buch 4", "Buch 5"]
+
 struct HomeView: View {
+    
+    @State private var settingsPresented = false
+    @State private var searchString = ""
+    
     var body: some View {
-        Text("HomeView")
+        NavigationStack {
+            // Liste Empfehlungen
+            // Hinzuzufügen
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Empfehlungen")
+                        .font(.title2)
+                        .bold()
+                        .padding(.leading)
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(bookList, id: \.self) { book in
+                                Text(book)
+                            }
+                            .frame(width: 96, height: 128)
+                            .background(.gray)
+                        }
+                        .padding(.horizontal)
+                    }
+                    // Eigene Bücherlisten
+                    Text("Deine Listen")
+                        .font(.title2)
+                        .bold()
+                        .padding(.leading)
+                    ForEach(bookList, id: \.self) { book in
+                        VStack(alignment: .leading) {
+                            Text(book)
+                                .padding(.horizontal, 8)
+                                .padding(.vertical, 32)
+//                                .background(.green)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.horizontal)
+                        }
+                    }
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+                }
+            }
+            .navigationTitle("Home")
+            .searchable(text: $searchString)
+            .onChange(of: searchString) {
+                // ViewModel Suchfunktion
+                
+            }
+            .toolbar {
+                Button("Einstellungen", systemImage: "gear") {
+                    settingsPresented.toggle()
+                }
+            }
+            .sheet(isPresented: $settingsPresented) {
+                // Einstellungen für die Startseite
+                Text("Einstellungen")
+                Button("Einstellungen schließen") {
+                    settingsPresented.toggle()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
