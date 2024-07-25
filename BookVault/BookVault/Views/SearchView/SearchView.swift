2e35b2865ff063cb79ca5b807c147411456486b2
moebius91 <fb.othersen@gmail.com>
2024-07-25 16:17:50 +0200

    Eigene Searchbar (mit Scanbutton) implementiert

--- BookVault/BookVault/Views/SearchView/SearchView.swift
+++ BookVault/BookVault/Views/SearchView/SearchView.swift
@@ -13,6 +13,9 @@ struct SearchView: View {
 
     @State private var searchString: String = ""
     @State private var selectedIndex = 0
+    @State private var isEditing: Bool = false
+
+    @FocusState private var isTextFieldFocused: Bool
 
     let searchOptions = ["ISBN", "Titel", "Autor"]
 
@@ -20,36 +23,97 @@ struct SearchView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
-                Picker("Search by", selection: $selectedIndex) {
-                    ForEach(0..<searchOptions.count, id: \.self) { index in
-                        Text(searchOptions[index])
+            VStack(spacing: 0) {
+                if !isEditing {
+                    Text("Suche")
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 16)
+                        .padding(.top, 44)
+                        .transition(.opacity)
+                        .animation(.easeInOut(duration: 0.5), value: isEditing)
+                }
+
+                HStack {
+                    ScannerSearchBar( text: $searchString, isEditing: $isEditing) {
+                        scannerViewModel.isScanning = true
+                        isEditing = true
+                        isTextFieldFocused = true
+                    }
+                    .transition(.move(edge: .bottom))
+                    .focused($isTextFieldFocused)
+                    .onSubmit {
+                        isEditing = true
+                        performSearch()
+                    }
+                    if isEditing {
+                        Button("Cancel") {
+                            withAnimation {
+                                isEditing = false
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                            }
+                        }
+                        .padding(.trailing, 10)
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing).combined(with: .move(edge: .bottom)).combined(with: .opacity),
+                            removal: .move(edge: .bottom).combined(with: .move(edge: .trailing)).combined(with: .opacity)
+                        ))
                     }
                 }
-                .pickerStyle(SegmentedPickerStyle())
                 .padding(.horizontal)
-            }
-            Spacer()
-            VStack {
-                if !searchString.isEmpty {
-                    switch selectedIndex {
-                    case 0:
-                        if viewModel.book != nil && searchString.isNumeric {
-                            HStack {
-                                Text("Suchergebnis:")
-                                    .font(.title2)
-                                    .bold()
-                                    .padding(.top, 4)
-                                    .padding(.leading)
+                .padding(.top, isEditing ? 0 : 10)
+                .animation(.easeInOut(duration: 0.3), value: isEditing)
+
+                if isEditing {
+                    VStack(spacing: 0) {
+                        Picker("Search by", selection: $selectedIndex) {
+                            ForEach(0..<searchOptions.count, id: \.self) { index in
+                                Text(searchOptions[index])
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .padding(.horizontal)
+                        .padding(.top, 10)
+                        .transition(.move(edge: .bottom).combined(with: .opacity))
+                        .animation(.easeInOut(duration: 0.3), value: isEditing)
+                    }
+                }
+                Spacer()
+                VStack {
+                    if !searchString.isEmpty {
+                        switch selectedIndex {
+                        case 0:
+                            if viewModel.book != nil && searchString.isNumeric {
+                                HStack {
+                                    Text("Suchergebnis:")
+                                        .font(.title2)
+                                        .bold()
+                                        .padding(.top, 4)
+                                        .padding(.leading)
+                                    Spacer()
+                                }
+                                SingleBookResultView()
+                                    .environmentObject(viewModel)
                                 Spacer()
                             }
-                            SingleBookResultView()
-                                .environmentObject(viewModel)
-                            Spacer()
-                        }
-                    case 1:
-                        if !viewModel.books.isEmpty {
-                            VStack {
+                        case 1:
+                            if !viewModel.books.isEmpty {
+                                VStack {
+                                    HStack {
+                                        Text("Suchergebnisse:")
+                                            .font(.title2)
+                                            .bold()
+                                            .padding(.top, 4)
+                                            .padding(.leading)
+                                        Spacer()
+                                    }
+                                    BooksResultView()
+                                        .environmentObject(viewModel)
+                                }
+                            }
+                        case 2:
+                            if !viewModel.authors.isEmpty {
                                 HStack {
                                     Text("Suchergebnisse:")
                                         .font(.title2)
@@ -58,98 +122,110 @@ struct SearchView: View {
                                         .padding(.leading)
                                     Spacer()
                                 }
-                                BooksResultView()
+                                AuthorsResultView()
                                     .environmentObject(viewModel)
                             }
+                        default:
+                            fatalError()
                         }
-                    case 2:
-                        if !viewModel.authors.isEmpty {
-                            HStack {
-                                Text("Suchergebnisse:")
-                                    .font(.title2)
-                                    .bold()
-                                    .padding(.top, 4)
-                                    .padding(.leading)
-                                Spacer()
-                            }
-                            AuthorsResultView()
-                                .environmentObject(viewModel)
-                        }
-                    default:
-                        fatalError()
                     }
                 }
-            }
-            .navigationTitle("Suche")
-            .searchable(text: $searchString, prompt: "Suche nach \(searchOptions[selectedIndex])")
-            .toolbar {
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        scannerViewModel.isScanning = true
-                    }) {
-                        Image(systemName: "barcode.viewfinder")
-                    }
-                }
-            }
-            .onSubmit(of: .search) {
-                switch selectedIndex {
-                case 0:
-                    searchString.count == 13 ? viewModel.getBookByIsbn(searchString) : nil
-                case 1:
-                    viewModel.getBooksByTitle(searchString)
-                case 2:
-                    viewModel.getAuthors(searchString)
-                default:
-                    fatalError()
-                }
-            }
-            .onChange(of: selectedIndex) {
-                if !searchString.isEmpty {
-                    switch selectedIndex {
-                    case 0:
-                        searchString = ""
-                        viewModel.book = nil
-                        viewModel.getBookByIsbn(searchString)
-                    case 1:
-                        searchString = ""
-                        viewModel.getBooksByTitle(searchString)
-                    case 2:
-                        searchString = ""
-                        viewModel.getAuthors(searchString)
-                    default:
-                        fatalError()
-                    }
-                }
-            }
-            .sheet(isPresented: $viewModel.showSafari) {
-                if let url = viewModel.book?.buyURL {
-                    SafariView(url: url)
-                }
-            }
-            .sheet(isPresented: $scannerViewModel.isScanning) {
-                Text("Klicke auf das entsprechende Kästchen:")
-                    .padding()
-                    .bold()
-                DocumentScannerView(viewModel: scannerViewModel, searchString: $searchString)
-                    .onAppear {
+    //            .navigationTitle(isEditing ? "" : "Suche")
+    //            .searchable(text: $searchString, prompt: "Suche nach \(searchOptions[selectedIndex])")
+    //            .toolbar {
+    //                ToolbarItemGroup(placement: .navigationBarTrailing) {
+    //                    Button(action: {
+    //                        scannerViewModel.isScanning = true
+    //                    }) {
+    //                        Image(systemName: "barcode.viewfinder")
+    //                    }
+    //                }
+    //            }
+//                .onSubmit(of: .search) {
+//                    switch selectedIndex {
+//                    case 0:
+//                        searchString.count == 13 ? viewModel.getBookByIsbn(searchString) : nil
+//                    case 1:
+//                        viewModel.getBooksByTitle(searchString)
+//                    case 2:
+//                        viewModel.getAuthors(searchString)
+//                    default:
+//                        fatalError()
+//                    }
+//                }
+//                .onChange(of: selectedIndex) {
+//                    if !searchString.isEmpty {
+//                        performSearch()
+//                    }
+//                }
+                .onChange(of: selectedIndex) {
+                    if !searchString.isEmpty {
                         switch selectedIndex {
                         case 0:
-                            scannerViewModel.startBarcodeScanner()
-                        case 1, 2:
-                            scannerViewModel.startTextScanner()
+                            viewModel.book = nil
+                            viewModel.authors = []
+                            viewModel.books = []
+                            viewModel.getBookByIsbn(searchString)
+                        case 1:
+                            viewModel.authors = []
+                            viewModel.books = []
+                            viewModel.getBooksByTitle(searchString)
+                        case 2:
+                            viewModel.authors = []
+                            viewModel.books = []
+                            viewModel.getAuthors(searchString)
                         default:
-                            break
+                            fatalError()
                         }
                     }
-                    .onDisappear {
-                        scannerViewModel.scannedText = searchString
-                        scannerViewModel.stopScanning()
+                }
+                .sheet(isPresented: $viewModel.showSafari) {
+                    if let url = viewModel.book?.buyURL {
+                        SafariView(url: url)
                     }
+                }
+                .sheet(isPresented: $scannerViewModel.isScanning) {
+                    Text("Klicke auf das entsprechende Kästchen:")
+                        .padding()
+                        .bold()
+                    DocumentScannerView(viewModel: scannerViewModel, searchString: $searchString)
+                        .onAppear {
+                            switch selectedIndex {
+                            case 0:
+                                scannerViewModel.startBarcodeScanner()
+                            case 1, 2:
+                                scannerViewModel.startTextScanner()
+                            default:
+                                break
+                            }
+                        }
+                        .onDisappear {
+                            scannerViewModel.scannedText = searchString
+                            scannerViewModel.stopScanning()
+                        }
+                }
             }
+        }
+    }
+
+    private func performSearch() {
+        switch selectedIndex {
+        case 0:
+            searchString.count == 13 ? viewModel.getBookByIsbn(searchString) : nil
+        case 1:
+            viewModel.getBooksByTitle(searchString)
+        case 2:
+            viewModel.getAuthors(searchString)
+        default:
+            fatalError()
         }
     }
 }
 
 #Preview {
     SearchView()
+}
+
+#Preview("NavigatorView") {
+    NavigatorView()
 }
--- BookVault/BookVault/Views/SearchView/SearchViewSubViews/ScannerSearchBar.swift
+++ BookVault/BookVault/Views/SearchView/SearchViewSubViews/ScannerSearchBar.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct ScannerSearchBar: View {
+    @Binding var text: String
+    @Binding var isEditing: Bool
+    var onScanButtonTapped: () -> Void
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextField("Suche", text: $text, onEditingChanged: { editing in
+                withAnimation {
+                    isEditing = editing
+                }
+            })
+            .padding(7)
+            .padding(.horizontal, 25)
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            .overlay {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundStyle(.gray)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 8)
+
+                    if !text.isEmpty {
+                        Button(action: {
+                            self.text = ""
+                        }) {
+                            Image(systemName: "multiply.circle.fill")
+                                .foregroundStyle(.gray)
+                                .padding(.trailing, 8)
+                        }
+                    }
+
+                    Button(action: {
+                        withAnimation {
+                            onScanButtonTapped()
+                        }
+                    }) {
+                        Image(systemName: "barcode.viewfinder")
+                            .foregroundStyle(.blue)
+                            .padding(.trailing, 8)
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ScannerSearchBar()
+    @State var text: String = ""
+    @State var isEditing: Bool = false
+
+    return ScannerSearchBar(text: $text, isEditing: $isEditing) {
+        print("Scan button tapped")
+    }
 }
